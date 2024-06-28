e025d16d6677ed1cc3f784a9534b531084ccfdc9
rogervalentim <valentim.roger33@gmail.com>
2024-06-28 19:01:28 -0300

    feat: added the search by user in the input

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,21 @@
+import { useState, FormEvent, ChangeEvent } from "react";
 import hero from "../assets/hero-image-github-profile.png";
 import search from "../assets/Search.svg";
+import { useGithub } from "../context/use-github";
 
 export function Header() {
+  const { fetchUserData } = useGithub();
+  const [username, setUsername] = useState<string>("");
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetchUserData(username);
+  };
+
   return (
     <header className="w-full">
       <div className="relative">
@@ -12,17 +26,21 @@ export function Header() {
         />
         <div className="absolute inset-0 flex justify-center pt-8">
           <div className="relative">
-            <div className="flex items-center">
-              <input
-                type="text"
-                name="name"
-                className="h-12 w-96 rounded-md bg-[#20293A] pl-12 pr-4 text-white placeholder-gray-400 outline-none focus:outline-none focus:ring-2 focus:ring-[#3662E3] lg:w-[500px]"
-                placeholder="username"
-              />
-              <button type="button" className="absolute bg-transparent pl-3">
-                <img src={search} alt="search icon" />
-              </button>
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="flex items-center">
+                <input
+                  type="text"
+                  name="username"
+                  className="h-12 w-96 rounded-md bg-[#20293A] pl-12 pr-4 text-white placeholder-gray-400 outline-none focus:outline-none focus:ring-2 focus:ring-[#3662E3] lg:w-[500px]"
+                  placeholder="username"
+                  value={username}
+                  onChange={handleInputChange}
+                />
+                <button type="submit" className="absolute bg-transparent pl-3">
+                  <img src={search} alt="search icon" />
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
